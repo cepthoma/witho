c891271f5b6ef4fb554ab03a4d89bbf1c40ae36f
cepthoma <150080060+cepthoma@users.noreply.github.com>
2024-09-26 11:42:43 -0300

    s

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function soma ( numero1,numero2 ){
     return numero1+numero2;
 }
 console.log( soma (1,2));
-function area Quadrado (lado){
+function areaQuadrado (lado){
     return lado**2
 }
 console.log(areaQuadrado(7))
