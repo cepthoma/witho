3f620ee4c91055c112de82f7019d8f0cee99b7fb
cepthoma <150080060+cepthoma@users.noreply.github.com>
2024-09-26 11:34:08 -0300

    f

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ function soma ( numero1,numero2 ){
     return numero1+numero2;
 }
 console.log( soma (1,2));
+function soma (numero1,numero1,numero1,numero1){
+    return numero1+numero1+numero1+numero1
+}
+console.log(soma(numero1,numero1,numero1,numero1))
