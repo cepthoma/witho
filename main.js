d8c5eb0e86962232369c90ac118ed5c8bb17cd75
cepthoma <150080060+cepthoma@users.noreply.github.com>
2024-09-26 11:58:19 -0300

    d

--- main.js
+++ main.js
@@ -5,6 +5,6 @@ function soma ( numero1,numero2 ){
 }
 console.log( soma (1,2));
 let areaQuadrado = parseInt(prompt('areaQuadrado'));
-function areaQuadrado (lado){
+function areaQuadrado(lado){
     return lado**2;
 }
