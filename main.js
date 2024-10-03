1fbe596fb37cd762ab5512ab9ee2c9012b6b59f2
cepthoma <150080060+cepthoma@users.noreply.github.com>
2024-10-03 11:25:30 -0300

    d

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-alert ('thomas e willian')
-function soma ( numero1,numero2 ){
-    return numero1+numero2;
-}
-console.log( soma (1,2));
-function areaQuadrado (lado){
-    return lado**2;
-}
-let area =parseInt(prompt('qual a area do Quadrado'));
-document.writeareaQuadrado
-console.log(areaQuadrado(area));
-let media=function(numero1,numero2){
-    return numero1+numero2/2
-}
+// alert ('thomas e willian')
+// function soma ( numero1,numero2 ){
+//     return numero1+numero2;
+// }
+// console.log( soma (1,2));
+// function areaQuadrado (lado){
+//     return lado**2;
+// }
+// let area =parseInt(prompt('qual a area do Quadrado'));
+// document.writeareaQuadrado
+// console.log(areaQuadrado(area));
+// let media=function(numero1,numero2){
+//     return numero1+numero2/2
+// }
+let estados =['AC','RR','AM','RO','PA','AP','MT','TO','MA','GO','DF','MS','PI','BA','MG','SP','PR','SC','RS','RJ','ES','CE','RN','PB','PE','AL','SE']
