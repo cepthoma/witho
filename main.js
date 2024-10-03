b3eae81f0b946b0264a726812ee5f464a88a085b
cepthoma <150080060+cepthoma@users.noreply.github.com>
2024-10-03 12:29:04 -0300

    f

--- main.js
+++ main.js
@@ -12,17 +12,24 @@
 // let media=function(numero1,numero2){
 //     return numero1+numero2/2
 // }
-let estados =['AC','RR','AM','RO','PA','AP','MT','TO','MA','GO','DF','MS','PI','BA','MG','SP','PR','SC','RS','RJ','ES','CE','RN','PB','PE','AL','SE']
-let i = 0; 
-while(i<estados.length)
-    {
-        console.log(i); 
-        // i=i+1;
-        console.log(estados[i]);
-        i++;
-    }
-    for (let i = 0; i < estados.length; i++) {
-        // const element = array[i];
-        console.log(i)
-        console.log(estados[i])
-    }
+// let estados =['AC','RR','AM','RO','PA','AP','MT','TO','MA','GO','DF','MS','PI','BA','MG','SP','PR','SC','RS','RJ','ES','CE','RN','PB','PE','AL','SE']
+// let i = 0; 
+// while(i<estados.length)
+//     {
+//         console.log(i); 
+//         // i=i+1;
+//         console.log(estados[i]);
+//         i++;
+//     }
+//     for (let i = 0; i < estados.length; i++) {
+//         // const element = array[i];
+//         console.log(i)
+//         console.log(estados[i])
+//     }
+let idade = 16;
+if (idade>16) {
+    console.log('já pode ser emancipado ')
+}
+else{
+console.log('não pode ser emancipado')
+}
