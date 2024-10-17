1e65942ce496e5581e0466f1ece5e48fa7c5f593
cepthoma <150080060+cepthoma@users.noreply.github.com>
2024-10-17 12:11:37 -0300

    t

--- main.js
+++ main.js
@@ -48,16 +48,5 @@ function criaCartao(categoria, pergunta, resposta) {
     </div>
     </div>
     `
-
-    let respostaEstaVisivel = false
-
-    function viraCartao() {
-        respostaEstaVisivel = !respostaEstaVisivel
-        cartao.classList.toggle('active', respostaEstaVisivel)
-    }
-    cartao.addEventListener('click', viraCartao)
-
-
     container.appendChild(cartao)
-
 }
