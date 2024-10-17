5b93da47d4f79e242d7fa1d31d1f1afcacb84bec
cepthoma <150080060+cepthoma@users.noreply.github.com>
2024-10-17 11:57:19 -0300

    d

--- perguntas.js
+++ perguntas.js
@@ -1,104 +1,103 @@
 criaCatao(
-    Língua Pirtuguesa
-    Qual foi o movimento literário que tinha como característica a idealização do amor?
-    Romantismo)
-    
-    criaCatao(
-    ‘Química’
-    ‘Quais são os fatores que alteram a velocidade de uma reação?’
-    ‘Temperatura, pressão, superfície de contato, concentração e catalisador’)
-    
-    criaCatao(
-    ‘História’
-    ‘Qual foi a batalha que marcou a primeira derrota da Alemanha na 2WW?’
-    ‘Batalha de Stalingrado’)
-    
-    criaCatao(
-    ‘Geografia’
-    ‘Qual é o bioma mais ameaçado no Brasil?’
-    ‘Cerrado’)
-    
-    criaCatao(
-    ‘Língua Portuguesa’
-    ‘Qual é a maior característica do Realismo?’
-    ‘Animalização do homem’)
-    
-    criaCartao(
-    'Química'
-    'Como analisar uma cadeia carbônica' 
-    'primeiro temos que analisar se é cíclica ou acíclica depois homogênea ou heterogênea, Ramificação ou Normal, Saturada ou Insaturada')
-    
-    criaCartao(
-    'História'
-    'Quais são as principais contribuições de Getúlio Vargas'
-    'Impôs o salário mínimo, carteira de trabalho, CLT e sindicatos’)
-    
-    criaCartao(
-    'Geografia'
-    'Áreas do planeta que apresentam maior concentração industrial'
-    'Américas, Europa,Ásia e Oceania')
-    
-    criaCartao(
-    'Física'
-    'Qual é a fórmula para velocidade'
-    'V=delta quantidada÷delta tempo’
-    
-    criaCartao(
-    ‘Biologia'
-    'Qual é a ordem das categorias taxonômicas'
-    'Reino,Filo,Classe,Ordem,Família, Gênero, Espécie')
-    
-    criaCartao(
-    'Química'
-    'Quais são as representações dos compostos orgânicos' 
-    ‘Fórmula estrutural plana, fórmula estrutural condensada, fórmula  estrutural espacial, BOND LINE’)
-    
-    criaCartao(
-    'Sociologia'
-    'Quais são as principais instituições sociais'
-    'Família, Escola,Igreja')
-    
-    criaCartao(
-    'História'
-    'Quando aconteceu a proclamação da República Brasileira'
-    '1889')
-    
-    criaCatao(
-    ‘Matemática’
-    ‘Como se calcula o IDH(saúde) de uma região?’
-    ‘(expectativa de vida - expectativa mínima / expectativa máxima - mínima’)
-    
-    criaCartao(
-    'Matemática'
-    'Qual é a fórmula da probabilidade '
-    'P(E)=n(E)÷n(S)')
-    
-    criaCatao(
-    ‘Matemática’
-    ‘Como se calcula o IDH?’
-    ‘IDH(saúde) - IDH(educação - IDH(renda) / 3’)
-    
-    criaCartao(
-    'Matemática'
-    'Qual a fórmula da relação trigonométrica fundamental'
-    'sen²+cos²=1')
-    
-    criaCatao(
-    ‘Biologia’
-    ‘Quais espécies são consideradas cobras hoje?’
-    ‘Naja e Cobra rei’)
-    
-    criaCartao(
-    'Matemática'
-    'Quais são os quadrantes da tangente'
-    ‘tg 0⁰ ou 360⁰=0
-    tg 90⁰ e 270⁰ = não existe
-    tg=180⁰=1')
-    
-    criaCartao(
-    'Matemática'
-    'Qual é a fórmula dos quadrantes'
-    '2⁰Q=180⁰ - ângulo 
-    3⁰Q=180⁰ + ângulo 
-    4⁰Q=360⁰ - ângulo')
-    
+‘Língua Pirtuguesa’
+‘Qual foi o movimento literário que tinha como característica a idealização do amor?’
+‘Romantismo’)
+
+criaCatao(
+‘Química’
+‘Quais são os fatores que alteram a velocidade de uma reação?’
+‘Temperatura, pressão, superfície de contato, concentração e catalisador’)
+
+criaCatao(
+‘História’
+‘Qual foi a batalha que marcou a primeira derrota da Alemanha na 2WW?’
+‘Batalha de Stalingrado’)
+
+criaCatao(
+‘Geografia’
+‘Qual é o bioma mais ameaçado no Brasil?’
+‘Cerrado’)
+
+criaCatao(
+‘Língua Portuguesa’
+‘Qual é a maior característica do Realismo?’
+‘Animalização do homem’)
+
+criaCartao(
+'Química'
+'Como analisar uma cadeia carbônica' 
+'primeiro temos que analisar se é cíclica ou acíclica depois homogênea ou heterogênea, Ramificação ou Normal, Saturada ou Insaturada')
+
+criaCartao(
+'História'
+'Quais são as principais contribuições de Getúlio Vargas'
+'Impôs o salário mínimo, carteira de trabalho, CLT e sindicatos’)
+
+criaCartao(
+'Geografia'
+'Áreas do planeta que apresentam maior concentração industrial'
+'Américas, Europa,Ásia e Oceania')
+
+criaCartao(
+'Física'
+'Qual é a fórmula para velocidade'
+'V=delta quantidada÷delta tempo’
+
+criaCartao(
+‘Biologia'
+'Qual é a ordem das categorias taxonômicas'
+'Reino,Filo,Classe,Ordem,Família, Gênero, Espécie')
+
+criaCartao(
+'Química'
+'Quais são as representações dos compostos orgânicos' 
+‘Fórmula estrutural plana, fórmula estrutural condensada, fórmula  estrutural espacial, BOND LINE’)
+
+criaCartao(
+'Sociologia'
+'Quais são as principais instituições sociais'
+'Família, Escola,Igreja')
+
+criaCartao(
+'História'
+'Quando aconteceu a proclamação da República Brasileira'
+'1889')
+
+criaCatao(
+‘Matemática’
+‘Como se calcula o IDH(saúde) de uma região?’
+‘(expectativa de vida - expectativa mínima / expectativa máxima - mínima’)
+
+criaCartao(
+'Matemática'
+'Qual é a fórmula da probabilidade '
+'P(E)=n(E)÷n(S)')
+
+criaCatao(
+‘Matemática’
+‘Como se calcula o IDH?’
+‘IDH(saúde) - IDH(educação - IDH(renda) / 3’)
+
+criaCartao(
+'Matemática'
+'Qual a fórmula da relação trigonométrica fundamental'
+'sen²+cos²=1')
+
+criaCatao(
+‘Biologia’
+‘Quais espécies são consideradas cobras hoje?’
+‘Naja e Cobra rei’)
+
+criaCartao(
+'Matemática'
+'Quais são os quadrantes da tangente'
+‘tg 0⁰ ou 360⁰=0
+tg 90⁰ e 270⁰ = não existe
+tg=180⁰=1')
+
+criaCartao(
+'Matemática'
+'Qual é a fórmula dos quadrantes'
+'2⁰Q=180⁰ - ângulo 
+3⁰Q=180⁰ + ângulo 
+4⁰Q=360⁰ - ângulo')
