97ae72727ba3a82dc21443e919421ef54349abb3
cepthoma <150080060+cepthoma@users.noreply.github.com>
2024-10-17 12:13:14 -0300

    s

--- perguntas.js
+++ perguntas.js
@@ -1,20 +1,25 @@
-criaCatao('Língua Portuguesa',
+criaCatao(
+'Língua Portuguesa',
 'Qual foi o movimento literário que tinha como característica a idealização do amor?',
 'Romantismo')
 
-criaCatao('Química',
+criaCatao(
+'Química',
 'Quais são os fatores que alteram a velocidade de uma reação?',
 'Temperatura, pressão, superfície de contato, concentração e catalisador')
 
-criaCatao('História',
+criaCatao(
+'História',
 'Qual foi a batalha que marcou a primeira derrota da Alemanha na 2WW?',
 'Batalha de Stalingrado')
 
-criaCatao('Geografia',
+criaCatao(
+'Geografia',
 'Qual é o bioma mais ameaçado no Brasil?',
 'Cerrado')
 
-criaCatao('Língua Portuguesa',
+criaCatao(
+'Língua Portuguesa',
 'Qual é a maior característica do Realismo?',
 'Animalização do homem')
 
